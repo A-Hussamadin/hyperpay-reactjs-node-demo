41d4ac1467866f801e00c06b1e9e9370c62325fd
Hussamadin Abushelha <h.abushelha@gmail.com>
2019-09-09 15:09:15 +0300

    removing server/node_modules

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,75 +2,100 @@ const express = require('express');
 var https = require('https');
 var querystring = require('querystring');
 const router = express.Router();
+const axios = require('axios');
 
-
-router.post('/checkout', function (req, res) {
-    request(function (responseData) {
-        console.log(responseData);
-        res.json(responseData);
-    });
-})
+router.post('/checkout', function(req, res) {
+  request(function(responseData) {
+    console.log(responseData);
+    res.json(responseData);
+  });
+});
 
 function request(callback) {
-    var path = '/v1/checkouts';
-    var data = querystring.stringify({
-        'authentication.userId': '8a8294174d0595bb014d05d829e701d1',
-        'authentication.password': '9TnJPc2n9h',
-        'authentication.entityId': '8a8294174d0595bb014d05d82e5b01d2',
-        'amount': '92.00',
-        'currency': 'EUR',
-        'paymentType': 'DB'
+  var path = '/v1/checkouts';
+  var data = querystring.stringify({
+    entityId: '8a82941750616e5a01506185ccc3007c',
+    amount: '92.00',
+    currency: 'SAR',
+    paymentType: 'DB',
+  });
+  var options = {
+    port: 443,
+    host: 'test.oppwa.com',
+    path: path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': data.length,
+      Authorization:
+        'Bearer OGE4Mjk0MTc1MDYwODIzYTAxNTA2MDg2NmE0ODAwMmN8WlI5eld5UlA=',
+    },
+  };
+  var postRequest = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      jsonRes = JSON.parse(chunk);
+      return callback(jsonRes);
     });
-    var options = {
-        port: 443,
-        host: 'test.oppwa.com',
-        path: path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': data.length
-        }
-    };
-    var postRequest = https.request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            jsonRes = JSON.parse(chunk);
-            return callback(jsonRes);
-        });
-    });
-    postRequest.write(data);
-    postRequest.end();
+  });
+  postRequest.write(data);
+  postRequest.end();
 }
 
-
-
-router.post('/result', function (req, res) {
-    console.log(req.body);
-    resultRequest(req.body.resourcePath, function (responseData) {
-        res.json(responseData);
-    });
-})
-
+router.post('/result', function(req, res) {
+  console.log(req.body);
+  resultRequest(req.body.resourcePath, function(responseData) {
+    res.json(responseData);
+  });
+});
 
 function resultRequest(resourcePath, callback) {
-    var path = resourcePath;
-    path += '?authentication.userId=8a8294174d0595bb014d05d829e701d1'
-    path += '&authentication.password=9TnJPc2n9h'
-    path += '&authentication.entityId=8a8294174d0595bb014d05d82e5b01d2'
-    var options = {
-        port: 443,
-        host: 'test.oppwa.com',
-        path: path,
-        method: 'GET',
-    };
-    var postRequest = https.request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            jsonRes = JSON.parse(chunk);
-            return callback(jsonRes);
-        });
+  var path = resourcePath;
+  path += '?entityId=8a82941750616e5a01506185ccc3007c';
+  //   var options = {
+  //     port: 443,
+  //     host: 'test.oppwa.com',
+  //     path: path,
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization:
+  //         'Bearer OGE4Mjk0MTc1MDYwODIzYTAxNTA2MDg2NmE0ODAwMmN8WlI5eld5UlA=',
+  //     },
+  //   };
+  //   var postRequest = https.request(options, function(res) {
+  //     res.setEncoding('utf8');
+  //     res.on('data', function(chunk) {
+  //       jsonRes = JSON.parse(chunk);
+  //       return callback(jsonRes);
+  //     });
+  //   });
+  //   postRequest.end();
+  const url = 'https://test.oppwa.com' + path;
+  axios
+    .get(url, {
+      headers: {
+        Authorization:
+          'Bearer OGE4Mjk0MTc1MDYwODIzYTAxNTA2MDg2NmE0ODAwMmN8WlI5eld5UlA=',
+      },
+    })
+    .then(function(response) {
+      // handle success
+      // console.log(response);
+      try {
+        resDate = JSON.parse(response);
+      } catch (e) {
+        resData = response;
+        // console.log(resData.data.id);
+      }
+
+      return callback(resData.data);
+    })
+    .catch(function(error) {
+      // handle error
+      //
+
+      console.log(error);
     });
-    postRequest.end();
 }
 
 module.exports = router;
